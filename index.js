0e1f6a69f1823a8628ac6dd4e35538259f881d64
xiaoyao <635796359@qq.com>
2019-11-25 13:57:56 +0800

    支持多队列管理且每个队列数据隔离，哪个队列有数据则自动处理数据

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const Queue = require('./lib/Queue');
+const Queue = require('./lib/queue_manager');
 
 module.exports = Queue;
--- test/index.js
+++ test/index.js
@@ -1,17 +1,26 @@
-const MyQueue = require('./my_queue');
+const Queue = require('../index');
+const testQueue = Queue.newQueue('test');
+const testQueue1 = Queue.newQueue('test1');
 
 (async ()=>{
+  testQueue.createdJob({a: 'test'});
+  // test设置任务处理及间隔
+  testQueue.process(1000, (job, done)=>{
+    console.log(`\r\nstart queue test process job:`, job.id, job.data);
+    return done();
+  });
 
-  // 设置任务处理及间隔
-  MyQueue.process(1000, (job, done)=>{
-    console.log("start process job:", job.id, job.data);
+  testQueue1.createdJob({a: 'test1'});
+  // test1设置任务处理及间隔
+  testQueue1.process(1000, (job, done)=>{
+    console.log(`\r\nstart queue test1 process job:`, job.id, job.data);
     return done();
   });
 
   // 添加任务到队列
   while(1){
-    console.log('========> add job to queue');
-    MyQueue.createdJob({a: 1});
+    console.log('========> add job to queue test');
+    testQueue.createdJob({a: 1});
     await (new Promise(res=>{
       setTimeout(()=>{
         res();
